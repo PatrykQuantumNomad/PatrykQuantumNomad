5955e4f203c42130750bc24b2bda7037a51d7f7b
Patryk Golabek <pgolabek@gmail.com>
2026-02-20 08:21:59 -0500

    adding new blog on arrow functions

--- ec.config.mjs
+++ ec.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('astro-expressive-code').AstroExpressiveCodeOptions} */
 export default {
-  themes: ['github-dark', 'github-light'],
-  themeCssSelector: (theme) => {
-    if (theme.name === 'github-dark') return '.dark';
-    if (theme.name === 'github-light') return ':root:not(.dark)';
-    return undefined;
-  },
+  themes: ['github-dark'],
   useDarkModeMediaQuery: false,
 };
